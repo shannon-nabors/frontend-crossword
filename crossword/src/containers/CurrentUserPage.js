1c778caebc0cdeff34980aa98d7d303c8d440bee
shannon-nabors <shannon.nabors@gmail.com>
2019-04-13 21:54:33 -0400

    user page shows both published and solved crosswords

--- crossword/src/containers/CurrentUserPage.js
+++ crossword/src/containers/CurrentUserPage.js
@@ -1,22 +1,49 @@
 import React, { Component, Fragment } from 'react'
+import { Menu, Segment, Container } from 'semantic-ui-react'
 import { connect } from 'react-redux'
 import PuzzleContainer from './PuzzleContainer'
 
 class CurrentUserPage extends Component {
+  state = {
+    menu: "Published"
+  }
+
+  handleMenuClick = (e, { name }) => {
+    this.setState({ menu: name })
+  }
+
   render() {
     return(
-      <Fragment>
-        <PuzzleContainer
-          puzzles={ this.props.puzzles }
-        />
-      </Fragment>
+      <Container>
+        <Menu attached='top' tabular>
+          <Menu.Item
+            name='Published'
+            active={this.state.menu === "Published"}
+            onClick={this.handleMenuClick}
+          />
+          <Menu.Item
+            name='Solved'
+            active={this.state.menu === "Solved"}
+            onClick={this.handleMenuClick}
+          />
+        </Menu>
+        <Segment
+          attached="bottom"
+          id="user-page-puzzles"
+        >
+          <PuzzleContainer
+            puzzles={ this.state.menu === "Published" ? this.props.userPuzzles : this.props.solvedPuzzles }
+          />
+        </Segment>
+      </Container>
     )
   }
 }
 
 const mapStateToProps = (state, ownProps) => {
   return {
-    puzzles: state.userPuzzles
+    userPuzzles: state.userPuzzles,
+    solvedPuzzles: state.solvedPuzzles
   }
 }
 
